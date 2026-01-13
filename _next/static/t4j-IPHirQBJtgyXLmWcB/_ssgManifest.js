1752f590944ba2393df3f2d096a0db28be769cc5
Fahed Alahmad <alahmadfahed@gmail.com>
2026-01-13 09:13:32 +0300

    Updates

---
+++ _next/static/t4j-IPHirQBJtgyXLmWcB/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F[locale]","\u002F[locale]\u002Fcards","\u002F[locale]\u002Fcharacters","\u002F[locale]\u002Fstory"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
